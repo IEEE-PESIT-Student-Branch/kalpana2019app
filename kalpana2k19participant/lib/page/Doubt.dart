fffd2af4835d7d1554a1b0c1223e14f38d1d61eb
Rajath S Shetty <rajathshetty1999@gmail.com>
2019-09-29 22:49:34 +0530

    Redesigned Help page

--- kalpana2k19participant/lib/page/Doubt.dart
+++ kalpana2k19participant/lib/page/Doubt.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import 'package:kalpana2k19participant/database.dart';
 import 'package:kalpana2k19participant/model/Doubt.dart';
+import 'package:kalpana2k19participant/widget/doubtTile.dart';
+
 class DoubtPage extends StatefulWidget {
   @override
   _DoubtPageState createState() => _DoubtPageState();
@@ -9,16 +12,20 @@ class DoubtPage extends StatefulWidget {
 
 class _DoubtPageState extends State<DoubtPage> {
   Database database;
-  
+  final _message = TextEditingController();
+  List<Doubt> doubtlist = [Doubt('_message', 0, '_teamid')];
+  DatabaseReference helpRef;
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     database = new Database();
     database.initState();
+    helpRef = database.gethelpRef();
   }
-  
+
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
     database.dispose();
   }
@@ -26,22 +33,87 @@ class _DoubtPageState extends State<DoubtPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Select your domain'),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color(0xFF8418B7),
+          Color(0xFF450289),
+        ])),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 62,
+            ),
+            Text(
+              'Ask the Mentor',
+              style: TextStyle(
+                fontFamily: 'kau',
+                color: Colors.white,
+                fontSize: 38,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(32))),
+              child: TextField(
+                controller: _message,
+                decoration: InputDecoration(
+                  hintText: "....",
+                  border: InputBorder.none,
+                ),
+                maxLines: 8,
+                maxLength: 75,
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            RaisedButton(
+              padding: EdgeInsets.fromLTRB(55, 17, 55, 17),
+              child: Text("Ask Question!"),
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              onPressed: () async {
+                load(context);
+                await database.postdoubt(context, new Doubt(_message.text, 0, database.teamid));
+                  Navigator.of(context).pop();
+              },
+            )
+          ],
+        ),
       ),
-      body: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text('AI'),
-            onPressed: () => _handelPressed('AI'),
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.menu),
+        onPressed: () => _viewposts(context),
       ),
     );
   }
 
-  void _handelPressed(String domain) {
-    _ackAlert(context, domain, database);
+  void _viewposts(BuildContext ctx) {
+    showModalBottomSheet(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        backgroundColor: Colors.white,
+        context: ctx,
+        builder: (BuildContext buildContext) {
+          return ListView.builder(
+            itemBuilder: (BuildContext ctx, int index) {
+              return Doubtcard(doubtlist[index]);
+            },
+            itemCount: doubtlist.length,
+          );
+        });
   }
 }
 
@@ -49,88 +121,11 @@ Future<void> load(BuildContext context) {
   return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
-        // return AlertDialog(
-        // backgroundColor: Colors.transparent,
-        //content:
         return Material(
             type: MaterialType.transparency,
             child: Container(
               alignment: Alignment.center,
               child: CircularProgressIndicator(),
-            )
-            );
-      });
-}
-
-Future<void> _ackAlert(BuildContext context, String domain,Database database) async {
-  final _message = TextEditingController();
-  String _domain = domain;
-  
-  return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          //title: Text('Description',textAlign: TextAlign.center,),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(32.0)),
-          ),
-          contentPadding: EdgeInsets.only(top: 10.0),
-          content: Container(
-            width: 300.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(
-                  'Description',
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Divider(
-                  color: Colors.grey,
-                  height: 4.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 30.0, right: 30.0),
-                  child: TextField(
-                    controller: _message,
-                    decoration: InputDecoration(
-                      hintText: "....",
-                      border: InputBorder.none,
-                    ),
-                    maxLines: 8,
-                    maxLength: 75,
-                  ),
-                ),
-                InkWell(
-                  child: Container(
-                    padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                    decoration: BoxDecoration(
-                      color: Colors.purple,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(32.0),
-                          bottomRight: Radius.circular(32.0)),
-                    ),
-                    child: Text(
-                      "Submit",
-                      style: TextStyle(color: Colors.white),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  onTap: () async {
-                    load(context);
-                    await database.postdoubt(context,new Doubt(_domain, _message.text, 0, database.teamid));
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pop();
-                  },
-                )
-              ],
-            ),
-          ),
-        );
+            ));
       });
 }
